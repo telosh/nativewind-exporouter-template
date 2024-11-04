e7604a5b0c688fba4d356e4afe2bf1e663b18da3
telosh <terumi.contact.web@gmail.com>
2024-11-04 14:49:50 +0900

    init tailwind and nativewind

--- app-example/(tabs)/index.tsx
+++ app-example/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import { Image, StyleSheet, Platform } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/Themed/ThemedText';
+import { ThemedView } from '@/components/Themed/ThemedView';
 
 export default function HomeScreen() {
   return (
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { ThemedText } from "@/components/Themed/ThemedText";
+import { ThemedView } from "@/components/Themed/ThemedView";
 
 export default function Index() {
   return (
-    <View
+    <ThemedView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <MaterialCommunityIcons size={48} name="tailwind" color="#1AB3BA" />
+      <ThemedText className="text-2xl font-bold underline">Hello, NativeWind!</ThemedText>
+    </ThemedView>
   );
 }
